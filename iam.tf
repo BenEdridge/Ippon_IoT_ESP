e627d60a7bcd1dfc01f04e9db03080207c7cf3eb
Ben Edridge <BenEdridge@users.noreply.github.com>
2018-07-23 18:10:11 +1000

    Updated IAM for Cognito authenticate and unauthenticated access

--- iam.tf
+++ iam.tf
@@ -20,7 +20,121 @@ resource "aws_iam_role" "role_iot" {
 EOF
 }
 
+resource "aws_iam_role" "cognito_authenticated" {
+  name = "cognito_authenticated"
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Principal": {
+        "Federated": "cognito-identity.amazonaws.com"
+      },
+      "Action": "sts:AssumeRoleWithWebIdentity",
+      "Condition": {
+        "StringEquals": {
+          "cognito-identity.amazonaws.com:aud": "${aws_cognito_identity_pool.cognito_pool.id}"
+        },
+        "ForAnyValue:StringLike": {
+          "cognito-identity.amazonaws.com:amr": "authenticated"
+        }
+      }
+    }
+  ]
+}
+EOF
+}
+
+resource "aws_iam_role" "cognito_unauthenticated" {
+  name = "cognito_unauthenticated"
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Principal": {
+        "Federated": "cognito-identity.amazonaws.com"
+      },
+      "Action": "sts:AssumeRoleWithWebIdentity",
+      "Condition": {
+        "StringEquals": {
+          "cognito-identity.amazonaws.com:aud": "${aws_cognito_identity_pool.cognito_pool.id}"
+        },
+        "ForAnyValue:StringLike": {
+          "cognito-identity.amazonaws.com:amr": "unauthenticated"
+        }
+      }
+    }
+  ]
+}
+EOF
+}
+
+resource "aws_iam_role_policy" "authenticated" {
+  name = "authenticated_policy"
+  role = "${aws_iam_role.cognito_authenticated.id}"
+
+  policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Action": [
+        "mobileanalytics:PutEvents",
+        "cognito-sync:*",
+        "cognito-identity:*",
+        "iot:*",
+        "s3:*"
+      ],
+      "Resource": [
+        "*"
+      ]
+    }
+  ]
+}
+EOF
+}
+
+resource "aws_iam_role_policy" "unauthenticated" {
+  name = "unauthenticated_policy"
+  role = "${aws_iam_role.cognito_unauthenticated.id}"
+
+  policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Action": [
+        "mobileanalytics:PutEvents",
+        "cognito-sync:*",
+        "cognito-identity:*",
+        "iot:*",
+        "s3:*"
+      ],
+      "Resource": [
+        "*"
+      ]
+    }
+  ]
+}
+EOF
+}
+
 resource "aws_iam_role_policy_attachment" "s3_policy_attachment" {
     role = "${aws_iam_role.role_iot.name}"
     policy_arn = "arn:aws:iam::aws:policy/AmazonS3FullAccess"
+}
+
+resource "aws_cognito_identity_pool_roles_attachment" "aws_unauthed_attachment" {
+  identity_pool_id = "${aws_cognito_identity_pool.cognito_pool.id}"
+  "roles" {
+    "authenticated" = "${aws_iam_role.cognito_authenticated.arn}"
+    "unauthenticated" = "${aws_iam_role.cognito_unauthenticated.arn}"
+  }
+
+
 }
